b2e79066c5d399f084a9ab59a2b26b683761e458
johnchafi <johnchafi@gmail.com>
2024-12-09 18:32:11 -0500

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+const nextConfig = {
+  images: {
+      domains: ['utfs.io', 'images.unsplash.com'],
+      remotePatterns:[
+          {
+              protocol: 'https',
+              hostname: 'utfs.io',
+              port : ''
+          }
+
+      ]
+  }
+};
+
+export default nextConfig;
